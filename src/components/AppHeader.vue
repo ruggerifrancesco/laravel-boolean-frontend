313da88ee764b32ae502dd7451f83f07fbd0e525
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-09-05 15:37:04 +0200

    Added routes to nav bar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,21 +1,21 @@
 <template>
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-        <a class="navbar-brand" href="#">Cocktails</a>
+        <router-link to="/" class="navbar-brand">Cocktails</router-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
         </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">
+                        <router-link to="/about" class="nav-link">
                             About-Us
-                        </a>
+                        </router-link>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">
+                        <router-link to="/contacts" class="nav-link">
                             Contacts
-                        </a>
+                        </router-link>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
